a35ac971053b0d4668696f13fca68d19b13c9215
C. Brenhin Keller <cbkeller@dartmouth.edu>
2026-02-03 16:44:43 -0500

    Add `foo` and `bar` functions

--- src/TestPackage2.jl
+++ src/TestPackage2.jl
@@ -1,5 +1,34 @@
 module TestPackage2
 
-# Write your package code here.
+    # Write your package code here
+    """
+    ```julia
+    foo(a,b)
+    ```
+    Add `a` and `b`
+
+    ### Examples
+    ```julia
+    julia> foo(1,1)
+    2
+    ```
+    """
+    foo(a,b) = a+b
+    export foo
+
+    """
+    ```julia
+    bar(a,b)
+    ```
+    Multiply `a` and `b`
+
+    ### Examples
+    ```julia
+    julia> bar(1,1)
+    2
+    ```
+    """
+    bar(a,b) = a*b
+    export bar
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,9 @@ using Test
 
 @testset "TestPackage2.jl" begin
     # Write your tests here.
+    @test foo(1,1) == 2
+    @test foo(2,3) == 5
+
+    @test bar(1,1) == 1
+    @test bar(2,3) == 6
 end
