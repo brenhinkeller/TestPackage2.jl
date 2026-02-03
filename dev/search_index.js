2b446794b7d64cee5e2215644de21a038a465b07
Documenter.jl <documenter@juliadocs.github.io>
2026-02-03 21:46:07 +0000

    build based on a35ac97

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#TestPackage2","page":"Home","title":"TestPackage2","text":"Documentation for TestPackage2.\n\n","category":"section"}]
+[{"location":"#TestPackage2","page":"Home","title":"TestPackage2","text":"Documentation for TestPackage2.\n\n","category":"section"},{"location":"#TestPackage2.bar-Tuple{Any, Any}","page":"Home","title":"TestPackage2.bar","text":"bar(a,b)\n\nMultiply a and b\n\nExamples\n\njulia> bar(1,1)\n2\n\n\n\n\n\n","category":"method"},{"location":"#TestPackage2.foo-Tuple{Any, Any}","page":"Home","title":"TestPackage2.foo","text":"foo(a,b)\n\nAdd a and b\n\nExamples\n\njulia> foo(1,1)\n2\n\n\n\n\n\n","category":"method"}]
 }
